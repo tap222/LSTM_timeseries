61baea323a31c8db4ef6602b3231a97ff76c09d9
tap222 <mtapas.mohanty95@gmail.com>
2017-10-20 17:15:52 +0530

    Update lstm_time_series.py

--- lstm_time_series.py
+++ lstm_time_series.py
@@ -1,3 +1,5 @@
+#For Refernce
+#https://machinelearningmastery.com/time-series-prediction-lstm-recurrent-neural-networks-python-keras/
 import matplotlib.pyplot as plt
 dataset = pandas.read_csv('international-airline-passengers.csv', usecols=[1], engine='python', skipfooter=3)
 plt.plot(dataset)
